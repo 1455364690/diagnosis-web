15e182f8e8371af25dd72ad8f217c7d429dbb8a4
sunjh <1455364690@qq.com>
2021-03-09 11:45:25 +0800

    trace

--- src/components/trace/Trace.vue
+++ src/components/trace/Trace.vue
@@ -67,45 +67,95 @@
       <el-col :span="2">
         <div class="grid-content bg-purple"></div>
       </el-col>
-      <el-col :span="12">
+      <el-col :span="20">
         <div class="grid-content ">
-          <el-row v-if="methodInfoList.length!=0" type="flex" class="row-bg" justify="space-between">
-            <el-pagination
-              background
-              :page-size="currentPageSize"
-              :current-page.sync="currentPageNum"
-              @current-change="currentPageChange"
-              layout="prev, pager, next"
-              :total="methodInfoTotalNum">
-            </el-pagination>
-          </el-row>
-          <el-row :gutter="10"
-                  style="margin-top: 1px;box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .1);border-radius: 4px"
-                  v-for="(item,index) in methodInfoList" :command="item" :key="index" type="flex" class="row-bg"
-                  justify="space-between">
-            <el-col :span="24">
-              <el-row :gutter="10" style="margin-bottom: 5px">
-                <el-col v-if="item.is_matched" :span="24" style="color: #409EFF">{{ item.class_method }}</el-col>
-                <el-col v-if="!item.is_matched" :span="24" style="color: #F56C6C">{{ item.class_method }}</el-col>
-              </el-row>
-              <el-row :gutter="10">
-                <el-col :span="3">
-                  <el-tag v-if="item.is_matched" effect="dark" type="info" style="margin-top: 7px">
-                    {{ item.consume_time }}&nbsp;ms
-                  </el-tag>
-                  <el-tag v-if="!item.is_matched" effect="dark" type="info" style="margin-top: 7px">暂无数据</el-tag>
-                </el-col>
-                <el-col :span="21" style="color: #909399;font-size: 15px;padding-top: 10px">{{item.log_time}}</el-col>
-              </el-row>
-            </el-col>
-          </el-row>
-        </div>
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple-light">
-          <el-row class="grid-content bg-purple">
+          <el-row>
+            <div class="panel panel-default">
+              <div class="panel-heading" style="background: rgba(153, 153, 153, 0.8)">用户访问序列</div>
+              <div class="panel-body">
+                <el-table
+                  v-if="methodInfoList!=null && methodInfoList.length !==0"
+                  ref="multipleTable"
+                  :data="methodInfoList"
+                  tooltip-effect="dark"
+                  style="width: 100%">
+                  <el-table-column
+                    type="expand"
+                    width="55" style="height: 80px;padding-top: 0;">
+                    <template slot-scope="props">
+                      <el-row :gutter="10" style="height: 50px">
+                        <el-col :span="1">&nbsp;</el-col>
+                        <el-col :span="2">用户Id：</el-col>
+                        <el-col :span="3">
+                          <span>{{props.row.userId}}</span>
+                        </el-col>
+                      </el-row>
+                      <el-row :gutter="10" style="height: 50px">
+                        <el-col :span="1">&nbsp;</el-col>
+                        <el-col :span="2">sessionId：</el-col>
+                        <el-col :span="3">
+                          <span>{{props.row.sessionId}}</span>
+                        </el-col>
+                      </el-row>
+                      <el-row :gutter="10" style="height: 50px">
+                        <el-col :span="1">&nbsp;</el-col>
+                        <el-col :span="2">执行耗时：</el-col>
+                        <el-col :span="3">
+                          <el-tag v-if="props.row.consumeTime != null" effect="dark" type="info" >
+                            {{ props.row.consumeTime }}&nbsp;ms
+                          </el-tag>
+                        </el-col>
+                      </el-row>
+                      <el-row :gutter="10" style="height: 50px">
+                        <el-col :span="1">&nbsp;</el-col>
+                        <el-col :span="2">执行时间：</el-col>
+                        <el-col :span="10" style="color: #909399;font-size: 15px;">
+                          {{ props.row.logTime }}
+                        </el-col>
+                      </el-row>
+                      <el-row :gutter="10" style="height: 50px">
+                        <el-col :span="1">&nbsp;</el-col>
+                        <el-col :span="2">接口名称：</el-col>
+                        <el-col :span="10" style="color: #909399;font-size: 15px;">
+                          {{ props.row.interfaceName }}
+                        </el-col>
+                      </el-row>
+                      <el-row :gutter="10" style="height: 50px">
+                        <el-col :span="1">&nbsp;</el-col>
+                        <el-col :span="2">http方法：</el-col>
+                        <el-col :span="10" style="color: #909399;font-size: 15px;">
+                          {{ props.row.httpMethod }}
+                        </el-col>
+                      </el-row>
+                    </template>
+                  </el-table-column>
+                  <el-table-column label="用户访问序列" style="height: 60px">
+                    <template slot-scope="props">
+                      <el-row :gutter="10" style="margin-bottom: 5px">
+                        <el-col :span="24" style="color: #409EFF">{{
+                            props.row.interfaceName
+                          }}
+                        </el-col>
+                      </el-row>
+                    </template>
 
+                  </el-table-column>
+                </el-table>
+                <el-row v-if="methodInfoList.length!=0" type="flex" class="row-bg" justify="space-between">
+                  <el-pagination
+                    background
+                    :page-size="currentPageSize"
+                    :current-page.sync="currentPageNum"
+                    @current-change="currentPageChange"
+                    layout="prev, pager, next"
+                    :total="methodInfoTotalNum">
+                  </el-pagination>
+                </el-row>
+
+              </div>
+            </div>
           </el-row>
+
         </div>
       </el-col>
       <el-col :span="2">
@@ -177,8 +227,18 @@ export default {
         .replace('{session_id}', this.currentSessionId)
         .replace('{page_size}', this.currentPageSize)
         .replace('{page_num}', this.currentPageNum)).then(res => {
-        this.methodInfoList = res.list
-        this.methodInfoTotalNum = res.total
+          var data = [
+            {interfaceName:'123123123123','consumeTime':'123123',userId:'123123123',httpMethod:'GET',sessionId:'123123123123',traceId:'123123123123',logTime:'123123'},
+            {interfaceName:'123123123123','consumeTime':'123123',userId:'123123123',httpMethod:'GET',sessionId:'123123123123',traceId:'123123123123',logTime:'123123'},
+            {interfaceName:'123123123123','consumeTime':'123123',userId:'123123123',httpMethod:'GET',sessionId:'123123123123',traceId:'123123123123',logTime:'123123'},
+            {interfaceName:'123123123123','consumeTime':'123123',userId:'123123123',httpMethod:'GET',sessionId:'123123123123',traceId:'123123123123',logTime:'123123'},
+            {interfaceName:'123123123123','consumeTime':'123123',userId:'123123123',httpMethod:'GET',sessionId:'123123123123',traceId:'123123123123',logTime:'123123'},
+
+          ]
+          //this.methodInfoList = res.list
+          this.methodInfoList = data
+          this.methodInfoTotalNum = res.total
+          console.log(this.methodInfoList)
       })
     },
     test() {
